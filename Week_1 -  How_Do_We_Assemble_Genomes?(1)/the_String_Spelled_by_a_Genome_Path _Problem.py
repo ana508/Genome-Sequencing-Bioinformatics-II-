761a04ddcd92e7ed2b23ca0f336dfddfbb6fb0dd
ana508 <56735766+ana508@users.noreply.github.com>
2020-03-11 22:58:35 +0400

    Add files via upload

---
+++ Week_1 -  How_Do_We_Assemble_Genomes?(1)/the_String_Spelled_by_a_Genome_Path _Problem.py
@@ -0,0 +1,15 @@
+inpt = open('C:\\Users\\Ani\\Downloads\\dataset_198_3.txt', 'r')
+output = open('C:\\Users\\Ani\\OneDrive\\Desktop\\output.txt', 'w')
+reads=[]
+
+for line in fin.readlines():
+    reads.append(line.replace('\n',''))
+
+k=len(reads[0])
+output.write(reads[0])
+
+for r in reads[1:]:
+    output.write(r[k-1:])
+    
+inpt.close()
+output.close()
